746f9f2bc9e4d9959d3e8b837b9fcbaaae67e8e7
devmaestra <127052615+devmaestra@users.noreply.github.com>
2023-08-24 19:59:56 -0500

    #U7_03 items

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
           />
         <Route
           path='/rooms'
-          element={<RoomIndex />}>
+          element={<RoomIndex token={sessionToken} />}>
         </Route>
         <Route>
           
